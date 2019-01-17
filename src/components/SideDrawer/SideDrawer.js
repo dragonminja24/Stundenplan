1c616f453c374b2c29096d61aa2683b60f0784d5
Pumba98 <mail@pumba98.de>
2019-01-17 17:28:48 +0100

    fix default group

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -24,7 +24,7 @@ class sideDrawer extends Component {
       this.state = {
         studiengang: "",
         semester: "",
-        gruppe: "",
+        gruppe: "alle",
         dbData: null,
         selectStudiengaenge: null,
         selectSemester: null,
